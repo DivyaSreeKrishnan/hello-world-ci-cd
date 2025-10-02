eecc52d53d1111bea76cf50c7b4387a156d7e762
Divya <dsree54321@gmail.com>
2025-10-02 11:01:33 +0530

    Hello world

---
+++ app.py
@@ -0,0 +1,5 @@
+def hello_world(): #defines the function.
+    return "Hello, World" #checks if this file is being run directly (not imported as a module)
+
+if __name__ == "__main__":
+    print(hello_world())
